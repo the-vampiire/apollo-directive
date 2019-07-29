dbe5c57bf471b3584fb39698dfdeac6bdf489995
Vamp <vampiirecodes@gmail.com>
2019-07-28 21:20:05 -0400

    createSchemaDirectives: initial draft

---
+++ src/create-schema-directives.js
@@ -0,0 +1,21 @@
+require("./jsdoc-types");
+const createDirective = require("./create-directive");
+
+/**
+ * @typedef {import('./jsdoc-types.js').DirectiveConfig} DirectiveConfig
+ */
+
+/**
+ * Creates an Apollo Server config.schemaDirectives object
+ * @param {{ directiveConfigs: [DirectiveConfig] }} config
+ * @returns {Object} schemaDirectives
+ */
+const createSchemaDirectives = config => config.directiveConfigs.reduce(
+  (schemaDirectives, directiveConfig) => ({
+    ...schemaDirectives,
+    [directiveConfig.directiveKey]: createDirective(directiveConfig),
+  }),
+  {},
+);
+
+module.exports = createSchemaDirectives;
