61cd508d489271b516d046879a12e1762c29f8fd
Vamp <vampiirecodes@gmail.com>
2019-07-29 21:48:38 -0400

    createDirective: refactored and added implementation notes
    - abstracted utilities into static and proto methods
    - support for edge case where Object and field have same directive applied
      - field level args take precedence (higher specificity) over object level args

--- src/create-directive.js
+++ src/create-directive.js
@@ -1,4 +1,4 @@
-/* eslint-disable class-methods-use-this, no-param-reassign, no-underscore-dangle */
+/* eslint-disable no-param-reassign, no-underscore-dangle */
 const { defaultFieldResolver } = require("graphql");
 const { SchemaDirectiveVisitor } = require("graphql-tools");
 
@@ -9,40 +9,91 @@ const { validateDirectiveConfig } = require("./utils");
  * @param {import('./jsdoc-types.js').DirectiveConfig} directiveConfig
  */
 const createDirective = (directiveConfig) => {
-  const { name, resolverReplacer, hooks = {} } = validateDirectiveConfig(
-    directiveConfig,
-  );
-  const { onVisitObject, onVisitFieldDefinition, onApplyDirective } = hooks;
+  const {
+    name,
+    resolverReplacer,
+    hooks: { onVisitObject, onVisitFieldDefinition, onApplyDirective } = {},
+  } = validateDirectiveConfig(directiveConfig);
 
   return class ApolloDirective extends SchemaDirectiveVisitor {
+    static markDirectiveApplied(directiveTarget) {
+      if (!directiveTarget._appliedDirectives) {
+        directiveTarget._appliedDirectives = {};
+      }
+
+      directiveTarget._appliedDirectives[name] = true;
+    }
+
+    static shouldApplyToField(directiveTarget, fromApplyToObject = false) {
+      if (!directiveTarget._appliedDirectives) {
+        directiveTarget._appliedDirectives = {};
+      }
+
+      const directiveAlreadyApplied = directiveTarget._appliedDirectives[name];
+
+      // if the directive is being applied from applyToObject and it has already been applied
+      if (fromApplyToObject && directiveAlreadyApplied) {
+        // edge case where both the Object Type and its field have been tagged by the same directive
+        // with different directive args, give directive args precedence to field level args
+        // as it has a higher specificity than the more general object level args
+        return false;
+      }
+
+      return true;
+    }
+
     visitObject(objectType) {
-      if (onVisitObject) onVisitObject(objectType);
-      this.applyDirective(objectType);
+      if (onVisitObject) {
+        onVisitObject(this.createDirectiveContext(objectType));
+      }
+
+      this.applyToObject(objectType);
     }
 
     visitFieldDefinition(field, details) {
-      if (onVisitFieldDefinition) onVisitFieldDefinition(field, details);
-      this.applyDirective(details.objectType);
+      const { objectType } = details;
+
+      if (onVisitFieldDefinition) {
+        onVisitFieldDefinition(this.createDirectiveContext(objectType, field));
+      }
+
+      this.replaceFieldResolver(objectType, field);
     }
 
-    applyDirective(objectType) {
-      if (onApplyDirective) onApplyDirective(objectType);
+    createDirectiveContext(objectType, field) {
+      return {
+        field,
+        objectType,
+        name: this.name,
+        args: this.args,
+      };
+    }
 
-      // exit early if the directive has already been applied to the object type
-      if (objectType[`_${name}DirectiveApplied`]) return;
-      objectType[`_${name}DirectiveApplied`] = true; // otherwise set _<key>DirectiveApplied flag
+    replaceFieldResolver(objectType, field, fromApplyToObject = false) {
+      // if the directive has already been applied to the field exit early
+      // prevents duplicate application of the directive
+      if (!ApolloDirective.shouldApplyToField(field, fromApplyToObject)) return;
 
+      if (onApplyDirective) {
+        onApplyDirective(this.createDirectiveContext(objectType, field));
+      }
+
+      // mapped scalar fields (without custom resolvers) will use the defaultFieldResolver
+      const originalResolver = field.resolve || defaultFieldResolver;
+
+      field.resolve = resolverReplacer(
+        originalResolver,
+        this.createDirectiveContext(objectType, field),
+      );
+
+      ApolloDirective.markDirectiveApplied(field);
+    }
+
+    applyToObject(objectType) {
       const fields = objectType.getFields();
 
       Object.values(fields).forEach((field) => {
-        // mapped scalar fields (without custom resolvers) will use the defaultFieldResolver
-        const originalResolver = field.resolve || defaultFieldResolver;
-
-        // replace the original resolver with the resolverWrapper returned from resolverReplacer
-        field.resolve = resolverReplacer(originalResolver, {
-          field,
-          objectType,
-        });
+        this.replaceFieldResolver(objectType, field, true);
       });
     }
   };
