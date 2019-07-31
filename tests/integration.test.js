7af0d4342ceef315a63207838fc7eca5fbd92738
Vamp <vampiirecodes@gmail.com>
2019-07-30 23:53:25 -0400

    integration tests: @auth on OBJECT and FIELD_DEFINITION tests complete

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,31 +1,72 @@
 const { graphql } = require("graphql");
 
-const {
-  onObject,
-  onCombined,
-  onFieldDefinition,
-} = require("./integration-test-cases");
+const { auth, upperCase } = require("./integration-test-cases");
 
 const runQueryTest = (schema, testCase) => {
-  const { message, query, expectedResult } = testCase;
+  const {
+    query,
+    message,
+    context = {},
+    expectedResult,
+    expectErrors = false,
+  } = testCase;
+
   test(message, async () => {
-    const response = await graphql(schema, query);
-    expect(response.data.result).toEqual(expectedResult);
+    const response = await graphql(schema, query, null, context);
+    if (expectErrors) {
+      expect(response.errors).toBeDefined();
+    } else {
+      expect(response.data.result).toEqual(expectedResult);
+    }
   });
 };
 
 describe("integration tests: applying schema directives and executing live queries", () => {
-  describe("on OBJECT: @upperCase on Person", () => {
-    const { schema, testCases } = onObject;
-    testCases.forEach(testCase => runQueryTest(schema, testCase));
+  describe("directive with no args: @upperCase", () => {
+    const { onObject, onCombined, onFieldDefinition } = upperCase;
+
+    describe("on OBJECT: @upperCase on Person", () => {
+      const { schema, testCases } = onObject;
+      testCases.forEach(testCase => runQueryTest(schema, testCase));
+    });
+
+    describe("on FIELD_DEFINITION: @upperCase on Person.name, Query.getString", () => {
+      const { schema, testCases } = onFieldDefinition;
+      testCases.forEach(testCase => runQueryTest(schema, testCase));
+    });
+
+    describe("on OBJECT | FIELD_DEFINITION: @upperCase on Person, Query.getString", () => {
+      const { schema, testCases } = onCombined;
+      testCases.forEach(testCase => runQueryTest(schema, testCase));
+    });
   });
 
-  describe("on FIELD_DEFINITION: @upperCase on Person.name, Query.getString", () => {
-    const { schema, testCases } = onFieldDefinition;
-    testCases.forEach(testCase => runQueryTest(schema, testCase));
-  });
-  describe("on OBJECT | FIELD_DEFINITION: @upperCase on Person, Query.getString", () => {
-    const { schema, testCases } = onCombined;
-    testCases.forEach(testCase => runQueryTest(schema, testCase));
+  describe("directive with args: @auth(require: [Role] = [ADMIN])", () => {
+    const {
+      onObject,
+      onCombined,
+      onFieldDefinition,
+      withConflictingArgs,
+    } = auth;
+
+    describe("on OBJECT: @auth (default [ADMIN]) on Person", () => {
+      const { schema, testCases } = onObject;
+      testCases.forEach(testCase => runQueryTest(schema, testCase));
+    });
+
+    describe("on FIELD_DEFINITION: @auth(require: [SELF, ADMIN]) on Person.name", () => {
+      const { schema, testCases } = onFieldDefinition;
+      testCases.forEach(testCase => runQueryTest(schema, testCase));
+    });
+
+    // describe("on OBJECT | FIELD_DEFINITION: @auth (default args) on Person, @auth(require: [SELF]) on Query.getMessages", () => {
+    //   const { schema, testCases } = onCombined;
+    //   testCases.forEach(testCase => runQueryTest(schema, testCase));
+    // });
+
+    // describe("on OBJECT | FIELD_DEFINITION with conflicting args: @auth(require: [USER]) on Person, @auth(require: [SELF, ADMIN]) on Person.email", () => {
+    //   const { schema, testCases } = withConflictingArgs;
+    //   testCases.forEach(testCase => runQueryTest(schema, testCase));
+    // });
   });
 });
